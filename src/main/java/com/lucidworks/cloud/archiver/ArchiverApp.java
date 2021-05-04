4ee5a1d2a95172c031b5d82ea6e7c746940f1c18
konrad <konrad.lys@lucidworks.com>
2021-05-04 09:34:23 +0200

    SEARCH-141: Init

---
+++ src/main/java/com/lucidworks/cloud/archiver/ArchiverApp.java
@@ -0,0 +1,11 @@
+package com.lucidworks.cloud.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiverApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiverApp.class, args);
+    }
+}
